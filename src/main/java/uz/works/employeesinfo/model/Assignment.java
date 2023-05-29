a1b2c27954e572376aaf15db442206b236f02d4e
Azizbek Matsalayev <matsalayev@outlook.com>
2023-05-29 09:52:55 +0500

    .

--- src/main/java/uz/works/employeesinfo/model/Assignment.java
+++ src/main/java/uz/works/employeesinfo/model/Assignment.java
@@ -1,4 +1,23 @@
 package uz.works.employeesinfo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Assignment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Integer durationDay;
 }
--- src/main/java/uz/works/employeesinfo/model/EmployeeTask.java
+++ src/main/java/uz/works/employeesinfo/model/EmployeeTask.java
@@ -1,4 +1,23 @@
 package uz.works.employeesinfo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class EmployeeTask {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer employeeId;
+    private Integer assignmentId;
 }
